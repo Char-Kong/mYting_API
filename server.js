590924ad9f0668e297baa043f3b278b4ffddacd6
KimJinSeong-Git <acmiheee@gmail.com>
2022-11-09 16:17:43 +0900

    feat: mvc

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+// Import Module
 const express = require('express');
-
 const app = express();
-const port = 3000;
 
-const Index = require('./routes/index');
-const Login = require('./routes/login');
-const Register = require('./routes/register');
 
-app.use('/', Index);
-app.use('/login', Login);
-app.use('/register', Register);
+// Routing
+const home = require('./routes/home');
 
-app.listen(port, () => {
-    console.log('서버 실행 .');
-})
+
+// App Setting
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', home);
+
+
+module.exports = app;
